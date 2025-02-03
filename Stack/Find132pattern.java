c2e81871adff834aeb08293907b5617f90439728
Lavishmunjal <0111lavishmunjal@gmail.com>
2025-02-03 22:26:06 +0530

    Leetcode 456 132 Pattern

---
+++ Stack/Find132pattern.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+
+public class Find132pattern {
+    public boolean find132pattern(int[] nums) {
+        int n = nums.length;
+        int nums3 = Integer.MIN_VALUE;
+        Stack<Integer> st = new Stack<>();
+        for(int i=n-1; i>=0; i--) {
+            if(nums[i]<nums3) {
+                return true;
+            }
+            while(!st.isEmpty() && st.peek()<nums[i]) {
+                nums3=st.pop();
+                
+            }
+            st.push(nums[i]);
+        }
+        return false;
+    }
+    
+}
