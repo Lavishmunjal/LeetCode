377a70a55a6bfae1beca5b9dd94bd4afb3d53f8d
Lavishmunjal <0111lavishmunjal@gmail.com>
2025-02-11 23:35:11 +0530

    Leetcode  1910 Remove All Occurrences of a Substring

---
+++ String/RemoveOccurrences.java
@@ -0,0 +1,16 @@
+public class RemoveOccurrences {
+    public String removeOccurrences(String s, String part) {
+        StringBuilder result = new StringBuilder();
+        int n = part.length();
+
+        for (char ch : s.toCharArray()) {
+            result.append(ch);
+
+            if (result.length() >= n && result.substring(result.length() - n).equals(part)) {
+                result.delete(result.length() - n, result.length());
+            }
+        }
+
+        return result.toString();
+    }
+}
